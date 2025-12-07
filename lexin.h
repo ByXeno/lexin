3ec89fd99e781cd19a53923f404fcc025f0629f5
Menderes Sabaz <sabazmenderes@gmail.com>
2025-12-07 14:41:11 +0300

    Added license to code

--- lexin.h
+++ lexin.h
@@ -670,3 +670,20 @@ bool lexin_consume_context
 
 #endif // LEXIN_IMPLEMENTATION
 
+/*
+ *  lexin - A simple lexer library in c
+ *  Copyright (C) 2025 Menderes Sabaz <sabazmenders@proton.me>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
